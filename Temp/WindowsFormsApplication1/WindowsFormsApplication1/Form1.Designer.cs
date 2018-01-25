e792a1d134dc6aec945b36055aa3c9ac6fd95850
LAB <LAB>
2018-01-25 14:32:54 -0600

    First exam

---
+++ Temp/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.scheduleDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.scheduleDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(96, 227);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "button1";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click_1);
+            // 
+            // scheduleDataGridView
+            // 
+            this.scheduleDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.scheduleDataGridView.Location = new System.Drawing.Point(12, 33);
+            this.scheduleDataGridView.Name = "scheduleDataGridView";
+            this.scheduleDataGridView.Size = new System.Drawing.Size(240, 150);
+            this.scheduleDataGridView.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.scheduleDataGridView);
+            this.Controls.Add(this.OpenButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.scheduleDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.DataGridView scheduleDataGridView;
+    }
+}
+
---
+++ Temp/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* Esc closes app */
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void OpenButton_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog fdial = new OpenFileDialog
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer),
+                Title = "Course schedule",
+                Filter = "Microsoft Excel 2007-2013 XML (*.xlsx)|*.xlsx|All Files (*.*)|*.*", //update this as file types are supported
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            // Fill DataTable with .xlsb file
+            if (fdial.ShowDialog() == DialogResult.OK)
+            {
+                if (fdial.FileName != null)
+                {
+                    try
+                    {
+                        string filename = fdial.FileName.ToString();
+                        string connectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + filename + ";" +
+                                                  "Extended Properties=\"Excel 12.0 Xml;HDR=YES;IMEX=1\""; //connectionstrings.com
+
+
+                        OleDbConnection connection = new OleDbConnection(connectionString); // establish link to file
+                        OleDbDataAdapter command = new OleDbDataAdapter("select * from [Sheet1$]", connection); // select every cell on Sheet1
+                        DataSet ds = new DataSet(); // initialize dataset for data grid
+                        command.Fill(ds); // fill data grid
+                        scheduleDataGridView.DataSource = ds.Tables[0];
+                        connection.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
